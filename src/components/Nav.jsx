9d030dcd125add8e0997e092b3b59f15dad3f876
HeatherRouke <heather214@gmail.com>
2024-03-20 16:30:05 -0400

    faq component structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,22 +15,27 @@ const Nav = () => {
       <nav>
         <div className="nav-wrapper">
           <img src="dog.png" className="brand-logo left" alt="logo" />
-          <NavLink to="/" className="nav-item right">Home</NavLink>
-          <div className="nav-item right">
-            <a href="#!" className="dropdown-trigger" data-target="about-dropdown">About<i className="material-icons right">arrow_drop_down</i></a>
-          </div>
-          <ul id="about-dropdown" className="dropdown-content">
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/reviews">Reviews</NavLink></li>
-            <li><NavLink to="/gallery">Gallery</NavLink></li>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li className="nav-item"><NavLink to="/">Home</NavLink></li>
+            <li className="nav-item"><NavLink to="/faq">FAQs</NavLink></li>
+            <li>
+              <a href="#!" className="dropdown-trigger nav-item" data-target="about-dropdown">About<i className="material-icons right">arrow_drop_down</i></a>
+              <ul id="about-dropdown" className="dropdown-content">
+                <li className="nav-item"><NavLink to="/about">About</NavLink></li>
+                <li className="nav-item"><NavLink to="/reviews">Reviews</NavLink></li>
+                <li className="nav-item"><NavLink to="/gallery">Gallery</NavLink></li>
+              </ul>
+            </li>
+            <li className="nav-item"><NavLink to="/services">Services</NavLink></li>
+            <li className="nav-item"><NavLink to="/contact">Reserve Your Spot</NavLink></li>
           </ul>
-          <NavLink to="/services" className="nav-item right">Services</NavLink>
-          <NavLink to="/contact" className="nav-item right">Reserve Your Spot</NavLink>
           <div className="nav-item right contact">Contact Directly (239)-292-5183</div>
         </div>
       </nav>
     </div>
   );
 };
+
+
 
 export default Nav;
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,7 +3,10 @@
 const Reviews = () => {
     return (
         <div>
-            <h1>Reviews</h1>
+            <h1 className="heading">Reviews</h1>
+            <div className="container reviews">
+                
+            </div>
         </div>
     )
 }
