51ec5d45a8e98bf47d6461c3b8ea112b89ddb641
HeatherRouke <heather214@gmail.com>
2024-03-21 13:52:13 -0400

    image gal component

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,12 +2,45 @@
 
 const ImageGallery = () => {
     return (
-        <div className="container image"> 
-            <h1 className="body-text">Image Gallery</h1>
-            <div className="carousel">
-                </div>
+      <div className="container image"> 
+        <h1 className="body-text">Image Gallery</h1>
+        <div className="carousel">
+          <a className="carousel-item" href="#one!">
+            <img src="https://i.imgur.com/sxWoLrA.jpeg" alt="dog" />
+          </a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/enl14Hf.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/dZgVM2n.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/1bHPurz.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/A6pUUdO.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PZOVLZy.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/5oozJ0H.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/DbV1HWV.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/FOX7iC9.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/ZB2pN8R.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PhHMJfZ.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/CA9pmWL.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/M1vVnte.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/vuIV5AX.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/cnVggnx.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/FX14mNW.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PNVj2Yv.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/7V8b1g1.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/IdUuq2B.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/RCXark8.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/hOgcWR8.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/lXjXRzj.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/TcFPUUm.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/wlyNXq8.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/bhwl7aQ.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/GD3eU3V.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/OfS8O9W.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/50bcbpJ.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/YXB3LFa.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/ovQNzvC.jpeg" alt="dog" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/97iF3zZ.jpeg" alt="dog" /></a>
         </div>
-    )
-}
-
-export default ImageGallery;
+      </div>
+    );
+  };
+  
+  export default ImageGallery;
