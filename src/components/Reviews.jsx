4d050dd685e44244247f3f74283c777285e3c530
HeatherRouke <heather214@gmail.com>
2024-03-21 15:42:58 -0400

    review component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,8 +4,14 @@ const Reviews = () => {
     return (
         <div>
             <h1 className="heading">Reviews</h1>
-            <div className="container reviews">
-                
+            <div className="container body-text reviews">
+                <div className="review">"Very nice property! Plenty of space for the dogs to enjoy playing around the ponds ,fountain and beautifully well maintained property. Guido is very friendly and seems quite knowledgeable. Made me feel very comfortable being there. My dog Cassie enjoyed her stay very much! They even sent me videos of the dogs playing." - William I</div>
+                <div className="review">"Guido and Mark are absolutely fantastic. They have a huge property for the dogs to run and the dogs are clearly treated like their own with lots of comfy areas to sleep and play. Their property is immaculate. As soon as I pulled onto their street the day after our initial meet and greet, my dog knew where he was and could barely wait to get back into their tropical oasis to see his new friends. He is now home, tired and very happy. I can’t recommend Mark and Guido highly enough. We will definitely use them again." - Mimi G</div>
+                <div className="review">"Absolutely will recommend Mark and Guido. Great people and took wonderful care of our Millie girl. They sent photos several times a day so you could see your furbaby was doing fine. You could see the genuine love of taking care of animals. Their home has an outdoor Oasis that no dog could refuse. Great experience, thanks guys!!" - Lynae M</div>
+                <div className="review">"The venue was way better than expected. Mark and Guido have a 5 acre jungle that is like a dog resort. Super friendly and sent us photos/videos almost daily. Nova came home extremely tired which is hard to do as a high energy breed. Their communication was great. Felt confident leaving our dog with strangers for the first time. Will use them again!" - Katie W</div>
+                <div className="review">"Mark and Guido are excellent with dogs. They have a beautiful, large fenced-in area for dogs to roam and play. They are friendly, easy-going, and they treated our pet, Sage, exactly as we would ... as family. Kudos to you, Mark and Guido!!" -Randy M.</div>
+                <div className="review">"Serenity... from start to finish and everything in between! Ringo is usually my shadow and always by my side (but not in a needy way). I had to call to him to come with me when we were ready to leave after our first meet and greet! Ringo was so very comfortable which of course made me so very comfortable which, in turn, made everyone around me so very comfortable while I was out of town! I never had to wonder about how Ringo was getting on because I received mesages and photos a couple of times each day. I think for me, the best part of Ringo's stay was that I didn't have to correct any of his behavior when we got home and the biggest validation that Ringo was as loved and cared for the whole time I was away." -Planet P</div>
+
             </div>
         </div>
     )
