55199ee5b3ea8c6b72513bcd688ccd0551d91725
HeatherRouke <heather214@gmail.com>
2024-03-22 14:07:12 -0400

    restructure image component

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -5,53 +5,117 @@ import M from "materialize-css";
 
 const ImageGallery = () => {
 
-
-useEffect(() => {
-    const carousels = document.querySelectorAll('.carousel');
-    M.Carousel.init(carousels, {
+    useEffect(() => {
+        const materialboxes = document.querySelectorAll('.materialboxed');
+        M.Materialbox.init(materialboxes, {
+          inDuration: 500,
+          outDuration: 500,
+          onOpenStart: function() {
+            console.log('An overlay has started opening!');
+          },
+          onCloseEnd: function() {
+            console.log('An overlay has finished closing!');
+          }
+        });
+      }, []); 
   
-      
-    })
-} , []);
-
-
     return (
-      <div className="container image-container"> 
+      <div> 
         <h1 className="body-text center">Image Gallery</h1>
-        <div className="carousel">
-          <a className="carousel-item" href="#one!">
-            <img src="https://i.imgur.com/sxWoLrA.jpeg" alt="dog" />
-          </a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/enl14Hf.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/dZgVM2n.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/1bHPurz.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/A6pUUdO.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PZOVLZy.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/5oozJ0H.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/DbV1HWV.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/FOX7iC9.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/ZB2pN8R.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PhHMJfZ.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/CA9pmWL.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/M1vVnte.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/vuIV5AX.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/cnVggnx.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/FX14mNW.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/PNVj2Yv.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/7V8b1g1.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/IdUuq2B.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/RCXark8.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/hOgcWR8.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/lXjXRzj.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/TcFPUUm.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/wlyNXq8.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/bhwl7aQ.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/GD3eU3V.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/OfS8O9W.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/50bcbpJ.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/YXB3LFa.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/ovQNzvC.jpeg" alt="dog" /></a>
-          <a className="carousel-item" href="#two!"><img src="https://i.imgur.com/97iF3zZ.jpeg" alt="dog" /></a>
+        <div className= "container image-gallery">
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/sxWoLrA.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/enl14Hf.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/dZgVM2n.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/1bHPurz.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/A6pUUdO.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/PZOVLZy.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/5oozJ0H.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/DbV1HWV.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/FOX7iC9.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/ZB2pN8R.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/PhHMJfZ.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/CA9pmWL.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/M1vVnte.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/vuIV5AX.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/cnVggnx.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/FX14mNW.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/PNVj2Yv.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/7V8b1g1.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/IdUuq2B.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/RCXark8.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/hOgcWR8.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/lXjXRzj.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/TcFPUUm.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/wlyNXq8.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/bhwl7aQ.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/GD3eU3V.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/OfS8O9W.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/50bcbpJ.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/YXB3LFa.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/ovQNzvC.jpeg" alt="dog" />
+        </div>
+        <div>
+          <img className="materialboxed" src="https://i.imgur.com/97iF3zZ.jpeg" alt="dog" />
+        </div>
         </div>
       </div>
     );
