b460915cf0fde43fa897ead395bb2b08e973f61b
HeatherRouke <heather214@gmail.com>
2024-04-29 16:46:09 -0400

    adjust pricing page

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,17 +5,21 @@ const Pricing = () => {
       <div className="container body-text center">
         <h1 className="heading">Pricing</h1>
         <div className="row">
-          <div className="col s6 detail-box ">
-        <h5>Long Term Boarding</h5>
-        <h2>$40</h2>
+          <div className="col s6">
+            <div className='card pricing-card'>
+        <h5 className="title">Long Term Boarding</h5>
+        <h2 className="price">$40</h2>
         <p className="pricing-detail">This cost covers 1 dog, per 24 hour period. Your pup will be cared for as if they are our own. All dogs sleep inside at night, not kept in cages and are fed on their regular schedule.</p>
-        <p>Available 7 days a week.</p>
+        <p className="pricing-detail">Available 7 days a week.</p>
         </div>
-        <div className="col s6 detail-box ">
-        <h5>Daycare</h5>
-        <h2>$30</h2>
+        </div>
+        <div className="col s6 ">
+          <div className="card">
+        <h5 className="title">Daycare</h5>
+        <h2 className="price">$30</h2>
         <p className="pricing-detail">This cost covers a drop off as early as 7:00am and pick-up by 6:00pm. Your pup will have open range of the farm to run, play and swim without being caged or leashed.</p>
-        <p>Available 7 days a week.</p>
+        <p className="pricing-detail">Available 7 days a week.</p>
+        </div>
         </div>
       </div>
 
